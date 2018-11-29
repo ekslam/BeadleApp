b1cedf0c75e464590ff8321ec8a812b8f4cc9cc9
Kevin <edwardkevinl1@gmail.com>
2018-11-29 09:46:11 +0800

    Add existing file

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { Platform } from 'ionic-angular';
 import { LoginPage } from '../../pages/login/login';
 import { TabsPage } from '../../pages/tabs/tabs';
@@ -12,8 +12,22 @@ export class HomePage {
 
   tab1Root = HomePage;
 
+  getInfo = {
+    name : '',
+    userPhoto: '',
+    email: '',
+    loggedin: false
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.getInfo.name = this.navParams.get('name')
+    this.getInfo.userPhoto = this.navParams.get('userPhoto')
+    this.getInfo.email = this.navParams.get('email')
+    this.getInfo.loggedin = this.navParams.get('loggedin')
+  }
+
+  ionViewLoad() {
+    console.log(this.getInfo.name + " " + this.getInfo.userPhoto + " " + this.getInfo.email + " " + this.getInfo.loggedin)
   }
 
 }
